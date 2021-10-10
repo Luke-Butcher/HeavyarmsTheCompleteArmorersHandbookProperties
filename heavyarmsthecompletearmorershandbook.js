4f7061647b5da169d53ba43b8894f860e0d398d0
Luke Butcher <butcher638@gmail.com>
2021-10-10 17:12:47 +0100

    Add files via upload

--- heavyarmsthecompletearmorershandbook.js
+++ heavyarmsthecompletearmorershandbook.js
@@ -1,6 +1,6 @@
 Hooks.on('ready', () => {
 	CONFIG.DND5E.weaponProperties['bandoiler'] = 'Bandoiler';
-	CONFIG.DND5E.weaponProperties['conceaked'] = 'Conceaked';
+	CONFIG.DND5E.weaponProperties['concealed'] = 'Concealed';
 	CONFIG.DND5E.weaponProperties['limited'] = 'Limited';
 	CONFIG.DND5E.weaponProperties['siege'] = 'Siege';
 
